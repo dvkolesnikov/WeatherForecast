cb7ecc4728c78e782d77f926b4bf56526f816ec2
Dmitry Kolesnikov <kolesnikov.nsk@gmail.com>
2026-03-17 23:15:25 +0100

    [AI] Add empty :feature-geocoding module

---
+++ feature-geocoding/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "com.example.feature_geocoding"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
---
+++ feature-geocoding/data/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.example.data.geocoding"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        minSdk = 24
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(project(":data-core"))
+    implementation(project(":domain-core"))
+    implementation(project(":feature-geocoding:domain"))
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.retrofit)
+
+    implementation(libs.koin.android)
+
+    testImplementation(libs.junit)
+}
